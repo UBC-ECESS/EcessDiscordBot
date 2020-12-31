ff6109c209cb0a785901673ade49075a613e0b3c
ShreyansK2000 <gshreyansk4@gmail.com>
2020-12-31 04:00:03 -0800

    added prerequisite command logic and input error checking

--- src/cogs/PrequisiteChecker.py
+++ src/cogs/PrequisiteChecker.py
@@ -3,6 +3,8 @@ Commands to verify prerequisites for ECE/CS courses
 """
 import discord
 from discord.ext import commands
+import csv
+import os
 
 """
 Cog for the prerequisite check commands
@@ -13,8 +15,40 @@ class PrerequisiteChecker(commands.Cog):
         self.client = client
 
     @commands.command()
-    async def ping(self, ctx):
-        await ctx.send("Pong!")
+    async def prereq(self, ctx, arg):
+
+        # A lot of error checking and log messages
+        if len(arg) != 7:
+            await ctx.send("Invalid Input")
+            return
+
+        program = arg[0:4]
+        if program != "cpen" and program != "elec":
+            await ctx.send("Unable to identify specified program")
+            return
+
+        courseNumString = arg[4:7]
+        try:
+            int(courseNumString)
+        except: 
+            await ctx.send("Invalid Course Number")
+            return          
+
+        # Parent directory of the bot repo; constructed as parentDir(srcDir(fileDir(file)))
+        botDir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+        course_info_file = open(os.path.join(botDir, 'assets/ece-course-prereqs.csv'), mode='r')
+
+        # Store course info csv as a collection of dictionaries
+        csv_reader = csv.DictReader(course_info_file, delimiter=',')
+        
+        for row in csv_reader:
+            # Send course information if matching course number found
+            if row['Course'].lower() == arg:
+                await ctx.send(f"Course Name: {row['Name']}\nPrerequisites: {row['Prerequisites']}\nCorequisites: {row['Corequisites']}\nCourse Page: {row['URL']}")
+                return
+
+        await ctx.send("Course Not Found")
+        return
 
 def setup(client):
     client.add_cog(PrerequisiteChecker(client))
