7f2fc9616fe26c0cfdf5b5d5ded357a8fd7c8105
Leslie Cheng <leslie.cheng5@gmail.com>
2021-01-08 15:28:08 -0800

    Use an embed for the course info.

--- src/cogs/PrequisiteChecker.py
+++ src/cogs/PrequisiteChecker.py
@@ -66,9 +66,11 @@ class PrerequisiteChecker(commands.Cog):
 
         if arg.upper() in self.course_info_dict:
             info = self.course_info_dict[arg.upper()]
-            await ctx.send(
-                f"Course: {info['Name']}\nPrerequisites: **{info['Prerequisites']}**\nCorequisites: **{info['Corequisites']}**\nCourse Description: {info['Description']}\nCourse Page: <{info['URL']}>"
-            )
+            em = discord.Embed(title=info["Name"], url=info["URL"])
+            em.add_field(name="Prerequisites", inline=False, value=info['Prerequisites'])
+            em.add_field(name="Corequisites",  inline=False, value=info['Corequisites'])
+            em.add_field(name="Description", inline=False, value=info["Description"])
+            await ctx.send(embed=em)
         else:
             await ctx.send("Course Not Found. Make sure input has no spaces.")
 
