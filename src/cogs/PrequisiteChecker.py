273e2151ddce05d0cd9c863916ed91c73b32b7e1
Leslie Cheng <leslie.cheng5@gmail.com>
2021-01-08 16:47:54 -0800

    Added courseinfo command, gave prereq an embed.

--- src/cogs/PrequisiteChecker.py
+++ src/cogs/PrequisiteChecker.py
@@ -3,8 +3,26 @@ Commands to verify prerequisites for ECE/CS courses
 """
 import discord
 from discord.ext import commands
+from bs4 import BeautifulSoup
 import csv
 import os
+import aiohttp
+import re
+
+
+class CourseConverter(commands.Converter):
+    async def convert(self, ctx, argument):
+        if len(argument) > 8:
+            raise commands.errors.BadArgument
+        match = re.search(r"([A-Z]{4})([0-9]{3}[A-Z]{0,1})", argument)
+        if match:
+            dept, course = match.groups()
+            if not dept or not course:
+                raise commands.errors.BadArgument
+            else:
+                return {"dept": dept, "course": course}
+        else:
+            raise commands.errors.BadArgument
 
 
 class PrerequisiteChecker(commands.Cog):
@@ -31,6 +49,7 @@ class PrerequisiteChecker(commands.Cog):
             course_info_dict[row["Course"]] = row
 
         self.course_info_dict = course_info_dict
+        self.session = aiohttp.ClientSession()
 
     """
     List the provided course's prerequisites and corequisites
@@ -67,12 +86,70 @@ class PrerequisiteChecker(commands.Cog):
         if arg.upper() in self.course_info_dict:
             info = self.course_info_dict[arg.upper()]
             em = discord.Embed(title=info["Name"], url=info["URL"])
-            em.add_field(name="Prerequisites", inline=False, value=info['Prerequisites'])
-            em.add_field(name="Corequisites",  inline=False, value=info['Corequisites'])
+            em.add_field(
+                name="Prerequisites", inline=False, value=info["Prerequisites"]
+            )
+            em.add_field(name="Corequisites", inline=False, value=info["Corequisites"])
             em.add_field(name="Description", inline=False, value=info["Description"])
             await ctx.send(embed=em)
         else:
             await ctx.send("Course Not Found. Make sure input has no spaces.")
+
+    @commands.command()
+    async def courseinfo(self, ctx, course: CourseConverter):
+        course_info = await self._scrape_course_info(
+            self._get_course_url(course["dept"], course["course"]), course
+        )
+        if course_info is None:
+            return await ctx.send("Course not found.")
+        em = discord.Embed(title=course_info["name"], url=course_info["url"])
+        em.set_footer(text="Always verify the results on the website.")
+        em.add_field(
+            name="Prerequisites", inline=False, value=course_info["prerequisites"]
+        )
+        em.add_field(
+            name="Corequisites", inline=False, value=course_info["corequisites"]
+        )
+        em.add_field(name="Description", inline=False, value=course_info["description"])
+        await ctx.send(embed=em)
+
+    @staticmethod
+    def _get_course_url(dept, course):
+        return f"https://courses.students.ubc.ca/cs/courseschedule?pname=subjarea&tname=subj-course&dept={dept}&course={course}"
+
+    async def _scrape_course_info(self, url, course):
+        try:
+            async with self.session.get(url) as resp:
+                soup = BeautifulSoup(await resp.text(), "html.parser")
+                name = soup.find(
+                    lambda predicate: predicate.name == "h4"
+                    and all([c in predicate.text for c in course.values()])
+                )
+                prereqs = soup.find(
+                    lambda predicate: predicate.name == "p"
+                    and "Pre-reqs:" in predicate.text
+                )
+                coreqs = soup.find(
+                    lambda predicate: predicate.name == "p"
+                    and "Co-reqs:" in predicate.text
+                )
+                if not name:
+                    return None
+                course_info = {
+                    "url": url,
+                    "name": name.text.strip(),
+                    "description": name.next_sibling.text.strip(),
+                    "prerequisites": prereqs.text.replace("Pre-reqs:", "").strip()
+                    if prereqs
+                    else "None",
+                    "corequisites": coreqs.text.replace("Co-reqs:", "").strip()
+                    if coreqs
+                    else "None",
+                }
+                return course_info
+        except Exception as e:
+            print(e)
+            return None
 
 
 def setup(client):
