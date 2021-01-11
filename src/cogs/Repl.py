8d23f9f103cdaa24fa8c60661df39bb0eb85abb4
Leslie Cheng <leslie.cheng5@gmail.com>
2021-01-10 18:06:17 -0800

    Restrict repl to guild only.

--- src/cogs/Repl.py
+++ src/cogs/Repl.py
@@ -37,6 +37,7 @@ class Repl(commands.Cog):
 
     @commands.command()
     @commands.max_concurrency(2)
+    @commands.guild_only()
     async def repl(self, ctx, language: str, code: Code):
         """Run code. Supports java, python, and javascript (node) as the language parameter.
         Code parameter should be in a code block."""
