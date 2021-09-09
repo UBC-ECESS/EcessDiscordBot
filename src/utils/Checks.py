47c93a70a19613a9b3fe9b23a5929519e64417c7
Leslie Cheng <leslie.cheng5@gmail.com>
2021-09-08 20:02:12 -0700

    Some small hotfixes for perms and threads.

--- src/utils/Checks.py
+++ src/utils/Checks.py
@@ -2,9 +2,8 @@ import discord
 from discord.ext import commands
 
 
-def ban_members_check(ctx: commands.Context):
-    return (
-        ctx.bot.is_owner(ctx.author)
-        or isinstance(ctx.author, discord.Member)
+async def ban_members_check(ctx: commands.Context):
+    return await ctx.bot.is_owner(ctx.author) or (
+        isinstance(ctx.author, discord.Member)
         and ctx.author.guild_permissions.ban_members
     )
